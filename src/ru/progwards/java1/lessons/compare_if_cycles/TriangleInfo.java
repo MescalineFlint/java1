f8c3ce29774389bd1b029de56f46f899eea6fd76
Дима <dimasic50@gmail.com>
2020-04-27 18:13:04 +0300

    TriangleSimpleInfo

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleInfo {
     public static boolean isTriangle(int a, int b, int c) {
-        boolean tre = a >= (b + c) || b >= (a + c) || c >= (a + b);
+        boolean tre = a < (b + c) & b < (a + c) & c < (a + b);
         return tre;
     }
 
