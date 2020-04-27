926afbcb8af72c6972d237a659df35c628ea804a
Дима <dimasic50@gmail.com>
2020-04-27 17:50:34 +0300

    TriangleSimpleInfo

--- src/ru/progwards/java1/lessons/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++
@@ -1,7 +0,0 @@
-package ru.progwards.java1.lessons.ru.progwards.java1.lessons.compare_if_cycles;
-
-public class TriangleInfo {
-    public static boolean isTriangle(int a, int b, int c){
-
-    }
-}
