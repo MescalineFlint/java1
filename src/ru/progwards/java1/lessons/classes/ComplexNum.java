ded65fe2172562daae87d207ae344801d80633e8
Дима <dimasic50@gmail.com>
2020-05-20 19:13:14 +0300

    TriangleSimpleInfo

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -1,28 +1,29 @@
-//package ru.progwards.java1.lessons.classes;
-//
-//public class ComplexNum {
-//    int a, b, z;
-//    public ComplexNum(int a, int b){
-//         z = a + b;
-//    }
-//    public String toString(){
-//
-//        return (a + b +"i");
-//    }
-//    public ComplexNum add(ComplexNum num){
-//
-//        return this.z + num.z = (this.a + num.a) + (this.b + num.b);
-//   }
-//    public ComplexNum sub(ComplexNum num){
-//
-//        return this.z - num.z = (this.a - num.a) + (this.b - num.b);
-//    }
-//    public ComplexNum mul(ComplexNum num) {
-//
-//        return this.z * num.z = (this.a * num.a - this.b * num.b) + (this.b * num.a + this.a * num.b);
-//    }
-//    public ComplexNum div(ComplexNum num){
-//
-//        return this.z / num.z = (this.a * num.a + this.b * num.b / num.a * num.a + num.b *num.b) + (this.b * num.a + this.a * num.b / num.a * num.a + num.b *num.b);
-//    }
-//}
+package ru.progwards.java1.lessons.classes;
+
+public class ComplexNum {
+    int a, b, z;
+    public ComplexNum(int a, int b){
+         ;
+    }
+    public String toString(){
+
+        return (a + b +"i");
+    }
+    public ComplexNum add(ComplexNum num){
+
+        return new ComplexNum(this.a + num.a, this.b + num.b);
+
+   }
+    public ComplexNum sub(ComplexNum num){
+
+        return new ComplexNum(this.a - num.a, this.b - num.b);
+    }
+    public ComplexNum mul(ComplexNum num) {
+
+        return new ComplexNum(this.a * num.a, this.b * num.b);
+    }
+    public ComplexNum div(ComplexNum num){
+
+        return new ComplexNum((this.a * num.a) + (this.b * num.b) / (num.a * num.a + num.b * num.b), ((this.b * num.a) - (this.a * num.b)) / (num.a * num.a + num.b * num.b));
+    }
+}
