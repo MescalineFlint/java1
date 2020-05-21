9c2c10e50993bbf565bf12b12ab9b936bcf2c9cd
Дима <dimasic50@gmail.com>
2020-05-21 21:08:26 +0300

    TriangleSimpleInfo

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -6,18 +6,12 @@ public class ArraySort {
         for (int i = 0; i <= a.length - 1; i++){
             for (int j = 1; j <= a.length - 1; j++) {
 
-                if (a[i] > a[i+1]){
+                if (a[i] > a[j]){
 
-                    int z = a[i];
-                    a[i] = a[i + 1];
-                    a[i + 1] = z;
+                    int x = a[i];
+                    a[i] = a[j];
+                    a[j] = x;
 
-                if (a[j] > a[j + 1]) {
-
-                    int q = a[j];
-                    a[j] = a[j + 1];
-                    a[j] = q;
-                    }
                 }
             }
         }
