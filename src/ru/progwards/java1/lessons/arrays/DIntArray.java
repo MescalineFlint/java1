3c3a8d79206f53c7e22bc043763e5a64f417b0ca
Дима <dimasic50@gmail.com>
2020-06-26 15:11:10 +0300

    TriangleSimpleInfo

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -5,45 +5,65 @@ import java.util.Arrays;
 public class DIntArray {
 
     private int a[];
-    int length;
+    int lenght;
 
-    public DIntArray() {
-        a = new int[10];
-        length = 0;
+    DIntArray (){
+        lenght = 0;
+        a = new int[0];
     }
-    public void doubleArray(){
-        int [] b = new int[a.length];
 
-    }
+
 
     public void add(int num){
-        if (length == a.length)
-            this.doubleArray();
 
-        int[] b = Arrays.copyOf(a, a.length * 2);
+
+        int[] b = Arrays.copyOf(a, a.length + 1);
         b[b.length -1] = num;
+        lenght++;
+        a = b;
+
 
 
     }
     public void atInsert(int pos, int num){
 
-        int[] b1 = Arrays.copyOf(a, a.length + 1);
-        b1[pos] = num;
+        int[] b = new int[lenght + 1];
+        b[pos] = num;
+        for (int i = 0; i < pos; i++) {
+            b[i] = a[i];
+        }
+        for (int i = pos; i < a.length; i++){
+                b[i + 1] = a[i];
+        }
+        lenght++;
+        a = b;
 
     }
     public void atDelete(int pos){
-        int x = a.length - 1;
-        a[a.length - 1] = a[pos];
-        a[pos] = x;
-        int[] b2 = Arrays.copyOf(a, a.length - 1);
+        int[] b = new int[lenght - 1];
+        for (int i = 0; i < pos; i++) {
+            b[i] = a[i];
+        }
+        for (int i = pos + 1; i < a.length; i++){
+            b[i - 1] = a[i];
+        }
+        lenght--;
+        a = b;
     }
     public int at(int pos){
         return a[pos];
     }
 
-    public static void main(String[] args)
-    {
-        int a[] = {2,4,6,7,9,23};
+    public static void main(String[] args) {
+       DIntArray a = new DIntArray();
+        a.add(2);
+        a.add(4);
+        a.add(6);
+        a.add(8);
+        a.atDelete(2);
+        a.atInsert(2,10);
+        System.out.println(a.at(2));
+
     }
 }
 
